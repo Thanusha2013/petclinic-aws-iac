7c7fab800be61e5b9322bb45e0f23704983140eb
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-15 15:02:50 +0530

    Update provider.tf

--- aws-terraform/provider.tf
+++ aws-terraform/provider.tf
@@ -4,23 +4,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"  # latest stable AWS provider
+      version = "~> 5.0"
     }
   }
 }
 
-
+# Default AWS provider
 provider "aws" {
   region = var.aws_region
 }
 
+# VPC resource
 resource "aws_vpc" "this" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
     Name = var.aws_vpc_name
   }
 }
-
-
