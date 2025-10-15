7189f3881d26897e9455412173d4c6927a36b590
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-15 19:40:01 +0530

    Update provider.tf

--- aws-terraform/provider.tf
+++ aws-terraform/provider.tf
@@ -12,3 +12,79 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+# Dynamic VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "spring-petclinic-vpc"
+  }
+}
+
+# Public subnets
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "spring-petclinic-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.aws_region}b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "spring-petclinic-public-subnet-2"
+  }
+}
+
+# ECS security group
+resource "aws_security_group" "ecs_sg" {
+  name        = "spring-petclinic-ecs-sg"
+  description = "ECS tasks SG"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Load Balancer security group
+resource "aws_security_group" "lb_sg" {
+  name        = "spring-petclinic-lb-sg"
+  description = "Load Balancer SG"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
