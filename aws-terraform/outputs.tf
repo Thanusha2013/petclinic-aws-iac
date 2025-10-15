958154ca7e16eeddea5fbf5fd140e391b138f6f5
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-15 18:49:30 +0530

    Update outputs.tf

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -37,7 +37,3 @@ output "ecs_cluster_name" {
 output "ecs_service_name" {
   value = aws_ecs_service.spring_petclinic_service.name
 }
-
-output "load_balancer_dns" {
-  value = aws_lb.spring_petclinic_lb.dns_name
-}
