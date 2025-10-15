5146235779f1bf38c04c6822571b80de6b2f68bf
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-15 20:15:06 +0530

    Update outputs.tf

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -7,37 +7,21 @@ output "public_subnets" {
 }
 
 output "ecs_security_group_id" {
-  value = aws_security_group.ecs_sg.id
+  value = aws_security_group.ecs.id
 }
 
-output "lb_security_group_id" {
-  value = aws_security_group.lb_sg.id
+output "alb_security_group_id" {
+  value = aws_security_group.alb.id
 }
 
-output "kms_key_id" {
-  value = aws_kms_key.spring_petclinic.id
+output "ecr_repo_url" {
+  value = aws_ecr_repository.spring_petclinic.repository_url
 }
 
-output "kms_key_arn" {
-  value = aws_kms_key.spring_petclinic.arn
+output "alb_dns_name" {
+  value = aws_lb.spring_petclinic.dns_name
 }
 
 output "s3_bucket_name" {
   value = aws_s3_bucket.spring_petclinic.bucket
-}
-
-output "s3_bucket_arn" {
-  value = aws_s3_bucket.spring_petclinic.arn
-}
-
-output "ecr_repository_url" {
-  value = aws_ecr_repository.spring_petclinic.repository_url
-}
-
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.spring_petclinic.name
-}
-
-output "load_balancer_dns" {
-  value = aws_lb.spring_petclinic.dns_name
 }
