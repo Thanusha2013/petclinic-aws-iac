77b2689f7fe0bf2273a7f10947a51d0dbc6631b6
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-15 20:11:18 +0530

    Update variables.tf

--- aws-terraform/variables.tf
+++ aws-terraform/variables.tf
@@ -3,18 +3,16 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
 variable "db_password" {
-  type      = string
-  default   = "MyS3cureP@ssw0rd!"  # You can change it
-  sensitive = true
+  type = string
+}
+
+variable "ecs_cpu" {
+  type    = string
+  default = "512"
+}
+
+variable "ecs_memory" {
+  type    = string
+  default = "1024"
 }
