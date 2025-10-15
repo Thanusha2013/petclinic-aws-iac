033870cf05b22a17cd6b52f19931e75488f419ea
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-15 19:40:19 +0530

    Update variables.tf

--- aws-terraform/variables.tf
+++ aws-terraform/variables.tf
@@ -1,34 +1,11 @@
 variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-
-variable "aws_vpc_name" {
-  type    = string
-  default = "spring-petclinic-vpc"
-}
-
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "ecs_security_group_name" {
-  type    = string
-  default = "ecs-sg"
-}
-
-variable "lb_security_group_name" {
-  type    = string
-  default = "lb-sg"
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "db_password" {
-  type    = string
   description = "Database password for the application"
+  type        = string
+  default     = "MyS3cureP@ssw0rd!"
 }
