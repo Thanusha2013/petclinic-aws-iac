fc6f1a67d58fa6d336e1c137f3118c818535f6dc
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-15 14:56:35 +0530

    Update variables.tf

--- aws-terraform/variables.tf
+++ aws-terraform/variables.tf
@@ -1,17 +1,24 @@
-variable "create_vpc" {
-  type        = bool
-  description = "Specify if you want to create a VPC. Accepted values are true or false."
-  default     = true
-}
-
 variable "aws_region" {
-  type        = string
-  description = "The AWS region where resources should be created, e.g., us-east-1, us-west-2."
-  default     = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
-variable "aws_vpc_name" {
-  type        = string
-  description = "The name tag for the AWS VPC."
-  default     = "spring-petclinic-vpc"
+variable "db_password" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "lb_security_group_id" {
+  type = string
+}
+
+variable "ecs_security_group_id" {
+  type = string
 }
