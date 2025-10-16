803ce9014d044a8a4f10cbe6088309abbe4848dc
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-16 12:05:15 +0530

    Update variables.tf

--- aws-terraform/variables.tf
+++ aws-terraform/variables.tf
@@ -1,18 +1,5 @@
 variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-
-variable "db_password" {
-  type = string
-}
-
-variable "ecs_cpu" {
-  type    = string
-  default = "512"
-}
-
-variable "ecs_memory" {
-  type    = string
-  default = "1024"
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
 }
