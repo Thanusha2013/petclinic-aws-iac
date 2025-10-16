2ff285ad1c419239c02a0a286f5ad26f55ba9b78
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-16 15:36:40 +0530

    Update outputs.tf

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -1,4 +1,36 @@
-output "load_balancer_dns_name" {
-  description = "Public DNS name of the Application Load Balancer"
-  value       = aws_lb.spring_petclinic.dns_name
+output "ecr_repo" {
+  description = "ECR repository URL for Spring PetClinic"
+  value       = aws_ecr_repository.spring_petclinic.repository_url
+}
+
+output "kms_key_id" {
+  value = aws_kms_key.spring_petclinic_init.id
+}
+
+output "kms_key_arn" {
+  value = aws_kms_key.spring_petclinic_init.arn
+}
+
+output "s3_bucket_id" {
+  value = aws_s3_bucket.spring_petclinic.id
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.spring_petclinic.arn
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.spring_petclinic.bucket
+}
+
+output "iam_role_name" {
+  value = aws_iam_role.spring_petclinic_role.name
+}
+
+output "iam_role_arn" {
+  value = aws_iam_role.spring_petclinic_role.arn
+}
+
+output "load_balancer_dns" {
+  value = aws_lb.spring_petclinic_lb.dns_name
 }
