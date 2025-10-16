a9903aa204f580b70492cd3eabe8cd28d164f97f
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-16 16:32:01 +0530

    Update outputs.tf

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -2,34 +2,14 @@ output "ecr_repo" {
   value = aws_ecr_repository.spring_petclinic.repository_url
 }
 
-output "kms_key_id" {
-  value = aws_kms_key.spring_petclinic_init.id
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.cluster.name
 }
 
-output "kms_key_arn" {
-  value = aws_kms_key.spring_petclinic_init.arn
-}
-
-output "s3_bucket_id" {
-  value = aws_s3_bucket.spring_petclinic.id
-}
-
-output "s3_bucket_arn" {
-  value = aws_s3_bucket.spring_petclinic.arn
-}
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.spring_petclinic.bucket
-}
-
-output "iam_role_name" {
-  value = aws_iam_role.spring_petclinic_role.name
-}
-
-output "iam_role_arn" {
-  value = aws_iam_role.spring_petclinic_role.arn
+output "ecs_service_name" {
+  value = aws_ecs_service.service.name
 }
 
 output "load_balancer_dns" {
-  value = aws_lb.spring_petclinic_lb.dns_name
+  value = aws_lb.lb.dns_name
 }
