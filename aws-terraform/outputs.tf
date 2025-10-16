6d344d19049008b308ecb727650a5a54e78cfdfa
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-16 12:06:48 +0530

    Update outputs.tf

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -1,27 +1,4 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnets" {
-  value = aws_subnet.public[*].id
-}
-
-output "ecs_security_group_id" {
-  value = aws_security_group.ecs.id
-}
-
-output "alb_security_group_id" {
-  value = aws_security_group.alb.id
-}
-
-output "ecr_repo_url" {
-  value = aws_ecr_repository.spring_petclinic.repository_url
-}
-
-output "alb_dns_name" {
-  value = aws_lb.spring_petclinic.dns_name
-}
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.spring_petclinic.bucket
+output "load_balancer_dns_name" {
+  description = "Public DNS name of the Application Load Balancer"
+  value       = aws_lb.spring_petclinic.dns_name
 }
