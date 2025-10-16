92b172f8804c18d77e26eae3b95e8d784d72a9cc
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-16 16:47:37 +0530

    Update outputs.tf

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -1,15 +1,19 @@
 output "ecr_repo" {
-  value = aws_ecr_repository.spring_petclinic.repository_url
-}
-
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.cluster.name
-}
-
-output "ecs_service_name" {
-  value = aws_ecs_service.service.name
+  description = "ECR repository URL for Spring PetClinic"
+  value       = aws_ecr_repository.spring_petclinic.repository_url
 }
 
 output "load_balancer_dns" {
-  value = aws_lb.lb.dns_name
+  description = "DNS name of the Load Balancer"
+  value       = aws_lb.spring_petclinic_lb.dns_name
+}
+
+output "ecs_cluster_name" {
+  description = "ECS Cluster name"
+  value       = aws_ecs_cluster.cluster.name
+}
+
+output "ecs_service_name" {
+  description = "ECS Service name"
+  value       = aws_ecs_service.service.name
 }
