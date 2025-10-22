67ea64417160beffcbff493dd7460eda33923e36
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-22 19:24:02 +0530

    Update variables.tf

--- aws-terraform/variables.tf
+++ aws-terraform/variables.tf
@@ -1,24 +1,18 @@
 variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "aws_vpc_name" {
-  default = "spring-petclinic-vpc"
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
 }
 
 variable "db_password" {
-  type      = string
-  sensitive = true
+  description = "Database password for RDS"
+  type        = string
 }
 
-variable "ecs_cpu" {
-  default = "512"
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
 }
 
-variable "ecs_memory" {
-  default = "1024"
-}
-
-variable "desired_count" {
-  default = 2
+variable "public_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
