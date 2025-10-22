b91c97c77224f281c98f340b99869ab132b8ae51
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-22 20:40:16 +0530

    Update provider.tf

--- aws-terraform/provider.tf
+++ aws-terraform/provider.tf
@@ -1,18 +1,13 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
+}
 
-  backend "s3" {
-    bucket = "spring-pet-clinic-01"
-    key    = "petclinic/terraform.tfstate"
-    region = "us-east-1"
-  }
+provider "aws" {
+  region = var.aws_region
 }
