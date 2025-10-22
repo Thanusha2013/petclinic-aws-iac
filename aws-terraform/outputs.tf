5414bea5bd259b9ac9bb5dd95ced2bb85ea4ab16
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-22 19:49:10 +0530

    Update outputs.tf

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -1,15 +1,4 @@
-output "load_balancer_dns" {
-  value = aws_lb.spring_petclinic_lb.dns_name
-}
-
-output "rds_endpoint" {
-  value = aws_db_instance.spring_petclinic_rds.address
-}
-
-output "rds_db_name" {
-  value = aws_db_instance.spring_petclinic_rds.db_name
-}
-
 output "ecr_repo" {
-  value = aws_ecr_repository.spring_petclinic.repository_url
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.spring_petclinic_repo.repository_url
 }
