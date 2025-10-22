c5f799f56504ecc92a5f91096dad04db6f76eaad
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-22 19:39:07 +0530

    Update variables.tf

--- aws-terraform/variables.tf
+++ aws-terraform/variables.tf
@@ -4,6 +4,14 @@ variable "aws_region" {
   default     = "ap-south-1"
 }
 
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
 variable "db_username" {
   description = "RDS master username"
   type        = string
@@ -14,12 +22,4 @@ variable "db_password" {
   description = "RDS master password"
   type        = string
   sensitive   = true
-}
-
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "public_subnets" {
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
