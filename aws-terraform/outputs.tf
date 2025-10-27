02098f32f261150089df78f8bc732ec8bdb4fd7f
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-27 14:23:29 +0530

    Update outputs.tf

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -1,14 +1,15 @@
-output "ecr_repo" {
-  description = "ECR Repository URL"
-  value       = aws_ecr_repository.app_repo.repository_url
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet" {
+  value = aws_subnet.public_a.id
+}
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.app_repo.repository_url
 }
 
 output "ecs_cluster_name" {
-  description = "ECS Cluster Name"
-  value       = aws_ecs_cluster.main.name
-}
-
-output "ecs_service_name" {
-  description = "ECS Service Name"
-  value       = aws_ecs_service.app.name
+  value = aws_ecs_cluster.main.name
 }
