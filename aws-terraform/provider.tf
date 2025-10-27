f930ab0bb229401885e7774f9c082f90ec38162d
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-27 14:26:19 +0530

    Update provider.tf

--- aws-terraform/provider.tf
+++ aws-terraform/provider.tf
@@ -1,14 +1,1 @@
-terraform {
-  required_version = ">= 1.8.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
