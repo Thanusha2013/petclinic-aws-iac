abb4a4c3b77ba446ac81ddf5f255f2bbc8249116
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-27 14:14:56 +0530

    Update variables.tf

--- aws-terraform/variables.tf
+++ aws-terraform/variables.tf
@@ -1,15 +1,4 @@
-variable "app_name" {
-  default = "spring-petclinic"
-}
-
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "public_subnet_a" {
-  default = "10.0.1.0/24"
-}
-
-variable "public_subnet_b" {
-  default = "10.0.2.0/24"
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
 }
