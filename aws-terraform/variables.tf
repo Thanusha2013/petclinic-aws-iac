ecf76bbedf730b098957a96e99892d3aeed9f3ad
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-27 12:31:58 +0530

    Update variables.tf

--- aws-terraform/variables.tf
+++ aws-terraform/variables.tf
@@ -1,3 +1,11 @@
 variable "project_name" {
-  default = "spring-petclinic"
+description = "Project name for tagging"
+type        = string
+default     = "spring-petclinic"
+}
+
+variable "app_port" {
+description = "Port that the container listens on"
+type        = number
+default     = 8080
 }
