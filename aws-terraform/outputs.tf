23872560d627d49008fb6bc85d384b0c7b880713
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-27 15:14:28 +0530

    Update outputs.tf

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -1,11 +1,23 @@
-output "ecr_repo_url" {
-  value = aws_ecr_repository.app_repo.repository_url
+output "vpc_id" {
+  value = aws_vpc.this.id
 }
 
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.main.name
+output "public_subnets" {
+  value = aws_subnet.public[*].id
 }
 
-output "ecs_service_name" {
-  value = aws_ecs_service.app_service.name
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.spring_petclinic_rds.endpoint
+}
+
+output "ecs_cluster_id" {
+  value = aws_ecs_cluster.spring_petclinic.id
+}
+
+output "ecr_repo" {
+  value = aws_ecr_repository.spring_petclinic.repository_url
 }
