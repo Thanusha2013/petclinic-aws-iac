4c32cd0ba48f7caf90242f9a06ba4c14764aecd8
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-27 13:26:47 +0530

    Update outputs.tf

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -14,6 +14,6 @@ output "ecs_cluster_name" {
   value = aws_ecs_cluster.main.name
 }
 
-output "ecs_service_name" {
-  value = aws_ecs_service.service.name
+output "ecs_task_definition" {
+  value = aws_ecs_task_definition.app.family
 }
