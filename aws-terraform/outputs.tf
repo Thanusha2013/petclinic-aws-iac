04caa087a7ac2dec67cceb974708a11c33c5e5b5
Thanusha2013 <129024451+Thanusha2013@users.noreply.github.com>
2025-10-27 12:52:02 +0530

    Update outputs.tf

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -1,11 +1,3 @@
-output "ecr_repo" {
-  value = aws_ecr_repository.repo.repository_url
-}
-
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.ecs_cluster.name
-}
-
 output "service_name" {
   value = aws_ecs_service.service.name
 }
